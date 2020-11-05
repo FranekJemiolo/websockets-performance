5e3f95aa193fa2b11452ff98b48d947a54ecf199
Franek Jemiolo <f.jemiolo@gmail.com>
2020-11-05 00:14:50 +0000

    Adding some options

--- websockets_client.py
+++ websockets_client.py
@@ -1,7 +1,11 @@
 import asyncio
+import base64
 import json
-import time
+import gzip
+import math
+from functools import partial
 
+import click
 import websockets
 from tqdm import tqdm
 
@@ -9,33 +13,71 @@ from tqdm import tqdm
 NUM_TEST_MESSAGES = 1000000
 
 
-async def performance_test():
+async def base_performance_test(handler):
     uri = "ws://localhost:8765"
     async with websockets.connect(uri, close_timeout=1, ping_interval=None, ping_timeout=None) as websocket:
         params = {"num_messages": 1000}
         params_str = json.dumps(params)
         await websocket.send(params_str)
-        for i in tqdm(range(NUM_TEST_MESSAGES)):
-            message = await websocket.recv()
+        await handler(websocket)
 
 
-async def performance_test_no_tqdm():
-    uri = "ws://localhost:8765"
-    print("No tqdm test")
-    async with websockets.connect(uri, close_timeout=1, ping_interval=None, ping_timeout=None) as websocket:
-        params = {"num_messages": 1000}
-        params_str = json.dumps(params)
-        await websocket.send(params_str)
-        before = time.time()
-        for i in range(NUM_TEST_MESSAGES):
-            message = await websocket.recv()
-        after = time.time()
-        print(f"Took {after - before} seconds to process {NUM_TEST_MESSAGES}")
+async def single_message_handler(websocket, num_messages=1000000, **kwargs):
+    for i in tqdm(range(num_messages)):
+        message = await websocket.recv()
 
 
-def main():
-    asyncio.get_event_loop().run_until_complete(performance_test())
-    asyncio.get_event_loop().run_until_complete(performance_test_no_tqdm())
+async def single_message_handler_gzip(websocket, num_messages=1000000, **kwargs):
+    for i in tqdm(range(num_messages)):
+        raw_message = await websocket.recv()
+        message = gzip.decompress(raw_message)
+
+
+async def batch_message_handler(websocket, num_messages=1000000, batch_size=100, **kwargs):
+    t = tqdm(total=num_messages)
+    for i in range(math.ceil(float(num_messages) / batch_size)):
+        raw_batch = await websocket.recv()
+        batch = json.loads(raw_batch)
+        messages = [base64.standard_b64decode(raw_message) for raw_message in batch]
+        t.update(n=batch_size)
+
+
+async def batch_message_handler_gzip(websocket, num_messages=1000000, batch_size=100, **kwargs):
+    t = tqdm(total=num_messages)
+    for i in range(math.ceil(float(num_messages) / batch_size)):
+        raw_batch = await websocket.recv()
+        batch = json.loads(gzip.decompress(raw_batch))
+        messages = [base64.standard_b64decode(raw_message) for raw_message in batch]
+        t.update(n=batch_size)
+
+
+async def performance_test(batch_size, compression, num_messages):
+    if batch_size == 1:
+        if compression == "GZIP":
+            await base_performance_test(partial(single_message_handler, num_messages=num_messages))
+        elif compression == "None":
+            await base_performance_test(partial(single_message_handler, num_messages=num_messages))
+    elif batch_size > 1:
+        if compression == "GZIP":
+            await base_performance_test(partial(single_message_handler, num_messages=num_messages,
+                                                batch_size=batch_size))
+        elif compression == "None":
+            await base_performance_test(partial(single_message_handler, num_messages=num_messages,
+                                                batch_size=batch_size))
+
+
+@click.command()
+@click.option("--batch-size",
+              help="Set the number of messages sent in one request",
+              default=1)
+@click.option("--compression",
+              help="Choose the type of compression for the message",
+              type=click.Choice(["None", "GZIP"], case_sensitive=False))
+@click.option("--num-messages",
+              help="Number of messages on which you want to test the client",
+              default=1000000)
+def main(batch_size, compression, num_messages):
+    asyncio.get_event_loop().run_until_complete(performance_test(batch_size, compression, num_messages))
 
 
 if __name__ == "__main__":
--- websockets_server.py
+++ websockets_server.py
@@ -1,18 +1,20 @@
 import asyncio
+import base64
+import gzip
+import json
+import random
 import signal
+import string
+from functools import partial
 
+import click
 import websockets
 
 
-MESSAGE_SIZE = 100
-
-
-async def send_indefinitely(websocket, path):
+async def basic_handler(websocket, path, handler=None, **kwargs):
     params = await websocket.recv()
-    random_message = "a" * MESSAGE_SIZE
     try:
-        while True:
-            await websocket.send(random_message)
+        await handler(websocket, path, params, **kwargs)
     except websockets.exceptions.ConnectionClosed:
         raise
     except KeyboardInterrupt:
@@ -20,15 +22,150 @@ async def send_indefinitely(websocket, path):
     finally:
         return
 
-async def run_server(stop_flag):
-    async with websockets.serve(send_indefinitely, "0.0.0.0", 8765, close_timeout=1, ping_interval=None, ping_timeout=None):
+
+# Deliberately implement copy-paste like functions to avoid if statements that could slow down the code
+# no compile time :(
+
+async def send_indefinitely_one(websocket, path, params, message_size=100, **kwargs):
+    random_message = "a" * message_size
+    while True:
+        await websocket.send(random_message)
+
+
+async def send_indefinitely_one_gzip(websocket, path, params, message_size=100, **kwargs):
+    random_message = "a" * message_size
+    while True:
+        await websocket.send(gzip.compress(random_message))
+
+
+async def send_indefinitely_one_random(websocket, path, params, message_size=100, **kwargs):
+    random.seed(42)
+    while True:
+        random_message = ''.join(random.choice(string.ascii_lowercase) for i in range(message_size))
+        await websocket.send(random_message)
+
+
+async def send_indefinitely_one_random_gzip(websocket, path, params, message_size=100, **kwargs):
+    random.seed(42)
+    while True:
+        random_message = ''.join(random.choice(string.ascii_lowercase) for i in range(message_size))
+        await websocket.send(gzip.compress(random_message))
+
+
+async def send_indefinitely_batch(websocket, path, params, batch_size=100, message_size=100, **kwargs):
+    random_message = "a" * message_size
+    counter = 0
+    batch = []
+    while True:
+        batch.append(base64.standard_b64encode(random_message))
+        counter += 1
+        if counter == batch_size:
+            await websocket.send(json.dumps(batch))
+            batch = []
+            counter = 0
+
+
+async def send_indefinitely_batch_gzip(websocket, path, params, batch_size=100, message_size=100, **kwargs):
+    random_message = "a" * message_size
+    counter = 0
+    batch = []
+    while True:
+        batch.append(base64.standard_b64encode(random_message))
+        counter += 1
+        if counter == batch_size:
+            await websocket.send(gzip.compress(json.dumps(batch)))
+            batch = []
+            counter = 0
+
+
+async def send_indefinitely_batch_random(websocket, path, params, batch_size=100, message_size=100, **kwargs):
+    random.seed(42)
+    counter = 0
+    batch = []
+    while True:
+        random_message = ''.join(random.choice(string.ascii_lowercase) for i in range(message_size))
+        batch.append(base64.standard_b64encode(random_message))
+        counter += 1
+        if counter == batch_size:
+            await websocket.send(json.dumps(batch))
+            batch = []
+            counter = 0
+
+
+async def send_indefinitely_batch_random_gzip(websocket, path, params, batch_size=100, message_size=100, **kwargs):
+    random.seed(42)
+    counter = 0
+    batch = []
+    while True:
+        random_message = ''.join(random.choice(string.ascii_lowercase) for i in range(message_size))
+        batch.append(base64.standard_b64encode(random_message))
+        counter += 1
+        if counter == batch_size:
+            await websocket.send(gzip.compress(json.dumps(batch)))
+            batch = []
+            counter = 0
+
+
+def get_handler_function(message_size, batch_size, random_message, compression):
+    if batch_size == 1:
+        if compression == "GZIP":
+            if random_message:
+                return partial(basic_handler, handler=send_indefinitely_one_random_gzip, batch_size=batch_size,
+                               message_size=message_size)
+            else:
+                return partial(basic_handler, handler=send_indefinitely_one_gzip, batch_size=batch_size,
+                               message_size=message_size)
+        elif compression == "None":
+            if random_message:
+                return partial(basic_handler, handler=send_indefinitely_one_random, batch_size=batch_size,
+                               message_size=message_size)
+            else:
+                return partial(basic_handler, handler=send_indefinitely_one, batch_size=batch_size,
+                               message_size=message_size)
+    elif batch_size > 1:
+        if compression == "GZIP":
+            if random_message:
+                return partial(basic_handler, handler=send_indefinitely_batch_random_gzip, batch_size=batch_size,
+                               message_size=message_size)
+            else:
+                return partial(basic_handler, handler=send_indefinitely_batch_gzip, batch_size=batch_size,
+                               message_size=message_size)
+        elif compression == "None":
+            if random_message:
+                return partial(basic_handler, handler=send_indefinitely_batch_random, batch_size=batch_size,
+                               message_size=message_size)
+            else:
+                return partial(basic_handler, handler=send_indefinitely_batch, batch_size=batch_size,
+                               message_size=message_size)
+
+
+async def run_server(stop_flag, message_size, batch_size, random_message, compression):
+    async with websockets.serve(get_handler_function(message_size, batch_size, random_message, compression),
+                                "0.0.0.0", 8765, close_timeout=1, ping_interval=None, ping_timeout=None):
         await stop_flag
 
 
-loop = asyncio.get_event_loop()
+@click.command()
+@click.option("--message-size",
+              help="Set the single message size",
+              default=100)
+@click.option("--batch-size",
+              help="Set the number of messages sent in one request",
+              default=1)
+@click.option("--random-message",
+              help="If you want to use randomly generated messages",
+              is_flag=True)
+@click.option("--compression",
+              help="Choose the type of compression for the message",
+              type=click.Choice(["None", "GZIP"], case_sensitive=False))
+def main(message_size, batch_size, random_message, compression):
+    loop = asyncio.get_event_loop()
 
-stop = loop.create_future()
-loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
+    stop = loop.create_future()
+    loop.add_signal_handler(signal.SIGTERM, stop.set_result, None)
 
-loop.run_until_complete(run_server(stop))
+    loop.run_until_complete(run_server(stop, message_size, batch_size, random_message, compression))
 
+
+if __name__ == "__main__":
+    main()
